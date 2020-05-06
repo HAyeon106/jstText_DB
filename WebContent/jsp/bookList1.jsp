7d136efcc14574f5d73eac67ed4b792b990603c6
Kim, Hayeon <gkdus02362@gmail.com>
2020-05-06 12:20:17 +0900

    1

--- WebContent/jsp/bookList1.jsp
+++ WebContent/jsp/bookList1.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ page %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,6 +8,36 @@
 <title>Insert title here</title>
 </head>
 <body>
+<div class="container">
 
+<h1>책목록</h1>
+
+<table>
+	<thead>
+		<tr>
+			<td>id</td>
+			<td>제목</td>
+			<td>저자</td>
+			<td>카테고리</td>
+			<td>가격</td>
+			<td>출판사</td>
+		</tr>
+	</thead>
+	
+	<tbody>
+		<% for() { %>
+		<tr>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+		</tr>
+		<% } %>
+	</tbody>
+</table>
+
+</div>
 </body>
 </html>
--- src/jspTest_DB/BookDAO.java
+++ src/jspTest_DB/BookDAO.java
@@ -1,5 +1,62 @@
 package jspTest_DB;
 
-public class BookDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
+public class BookDAO {
+	private static Book getBookFrom(ResultSet resultSet) throws SQLException{
+		Book book = new Book();
+		book.setId(resultSet.getInt("id"));
+		book.setTitle(resultSet.getString("title"));
+		book.setAuthor(resultSet.getString("author"));
+		book.setCategoryId(resultSet.getInt("categoryId"));
+		book.setPrice(resultSet.getInt("price"));
+		book.setPublisher(resultSet.getString("publisher"));
+		book.setCategoryName(resultSet.getString("categoryName"));
+		return book;
+	}
+
+	public static List<Book> findAll() throws Exception{
+		String sql = "SELECT b.*, c.categoryName "
+				+ "FROM book b LEFT JOIN category c ON b.categoryId = c.id";
+		try(Connection connection = DB.getConnection("student1");
+			 PreparedStatement statement = connection.prepareStatement(sql);
+			 ResultSet resultSet = statement.executeQuery()){
+			ArrayList<Book> list = new ArrayList<Book>();
+			while(resultSet.next())
+				list.add(getBookFrom(resultSet));
+			return list;
+		}
+	}
+	
+	public static List<Book> findByName(String name) throws Exception{
+		String sql= "SELECT b.*, c.categoryName "
+				+ "FROM book b LEFT JOIN category c ON b.categoryId = c.id";
+		try(Connection connection = DB.getConnection("student1");
+			 PreparedStatement statement = connection.prepareStatement(sql)){
+			statement.setString(1, name+"%");
+			try(ResultSet resultSet = statement.executeQuery()) {
+				ArrayList<Book> list = new ArrayList<Book>();
+				while(resultSet.next())
+					list.add(getBookFrom(resultSet));
+				return list;
+			}
+		}
+	}
+	
+	public static Book findById(int id) throws Exception{
+		String sql= "SELECT b.*, c.categoryName "
+				+ "FROM book b LEFT JOIN category c ON b.categoryId = c.id";
+		try(Connection connection = DB.getConnection("student1");
+			 PreparedStatement statement = connection.prepareStatement(sql)){
+			statement.setInt(1, id);
+			try(ResultSet resultSet = statement.executeQuery()){
+				return resultSet.next()?getBookFrom(resultSet):null;
+			}
+		}
+	}
 }
